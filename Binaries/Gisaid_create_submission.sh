c34eff9ad04970c60baa1be569295edde6160e26
jonbra <jon.brate@fhi.no>
2022-01-07 14:13:15 +0100

    updated to version creating an array

--- Binaries/Gisaid_create_submission.sh
+++ Binaries/Gisaid_create_submission.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 
 # Wrapper script for running "create_Gisaid_submission_files.R"
+# Save a copy of this file and keep it with the submission files for references.
 # Example run:
-# declare -a array=("FHI200" "FHI201" "FHI202")
-# for oppsett in "${array[@]}"; do ./Binaries/create_gisaid_submission.sh -p "Swift_FHI" -o "${oppsett}" -f "${oppsett}".fasta -m "${oppsett}".csv -S "jonbra"; done
+# ./Gisaid_create_submission.sh
+
+# Change the "oppsett" below. E.g. "FHI200", or "681" for Run681
+declare -a array=("FHI258")
+platform="Swift_FHI"
+submitter="jonbra"
 
 # loop through arguments
-while getopts p:o:f:m:S: flag
-do
-    case "${flag}" in
-        p) platform=${OPTARG};;
-        o) oppsett=${OPTARG};;
-        f) fasta=${OPTARG};;
-        m) metadata=${OPTARG};;
-        S) submitter=${OPTARG};;
-    esac
-done
-echo "Platform: $platform";
-echo "Oppsett: $oppsett";
-echo "Fasta filename: $fasta";
-echo "Metadata filename: $metadata";
-echo "Username submitter: $submitter";
+#while getopts p:o:f:m:S: flag
+#do
+#    case "${flag}" in
+#        p) platform=${OPTARG};;
+#        o) oppsett=${OPTARG};;
+#        f) fasta=${OPTARG};;
+#        m) metadata=${OPTARG};;
+#        S) submitter=${OPTARG};;
+#    esac
+#done
+#echo "Platform: $platform";
+#echo "Oppsett: $oppsett";
+#echo "Fasta filename: $fasta";
+#echo "Metadata filename: $metadata";
+#echo "Username submitter: $submitter";
 
 # make tmp directory and go to it
 mkdir -p ~/tmp_gisaid/Frameshift
 cd ~/tmp_gisaid/
 
 # Run R script
-docker run -it --rm \
+for oppsett in "${array[@]}"; do \
+  docker run -it --rm \
   -v /mnt/N/Virologi/:/home/docker/N \
   -v $(pwd):/home/docker/Fastq \
   garcianacho/fhisc2:Illumina \
-  Rscript --vanilla /home/docker/Scripts/Gisaid_create_submission_files.R -p $platform -o $oppsett -f $fasta -m $metadata -S $submitter
+  Rscript --vanilla /home/docker/Scripts/Gisaid_create_submission_files.R -p ${platform} -o "${oppsett}" -f "${oppsett}.fasta" -m "${oppsett}.csv" -S ${submitter}; done
+
+now=`date +"%Y-%m-%d"`
+declare -a array=(*.csv)
+head -1 "${array[0]}" > ${now}.csv
+for oppsett in "${array[@]}"; do grep -v "^covv" "${oppsett}" >> ${now}.csv; done
+for oppsett in "${array[@]}"; do cat "${oppsett%csv}fasta" >> ${now}.fasta; done
+#head -1 ${array[1]} > date.csv
+#for oppsett in "${array[@]}"; do grep -v "^covv" "${oppsett}.csv" >> date.csv; done
+#for oppsett in "${array[@]}"; do cat "${oppsett}.fasta" >> date.fasta; done
