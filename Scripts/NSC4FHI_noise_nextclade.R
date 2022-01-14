04c8d28c88432c0cc02f71c474e7a361f4e33bca
garcia-nacho <garcia.nacho@gmail.com>
2022-01-14 11:16:17 +0100

    Script to include noise colums on summaries

---
+++ Scripts/NSC4FHI_noise_nextclade.R
@@ -0,0 +1,21 @@
+library(readxl)
+starting.folder<-"/home/docker/Fastq/"
+df<-read.table(paste(starting.folder,"results/nextclade_for_FHI.tsv",sep = ""),sep = "\t")
+
+if(paste(rownames(df),collapse = "")!=paste(c(1:nrow(df)), collapse="")){
+  df<-cbind(rownames(df), df)
+  rownames(df)<-NULL
+  colnames(df)<-colnames(df)[-1]
+  df<-df[,c(1:15)]
+}
+
+noise.file<-list.files(paste(starting.folder,"/results/2_bam/noiseextractor/",sep = ""), pattern = "ResultsNoisExtractor.*\\.xlsx", full.names = TRUE)
+
+df2<-read_xlsx(noise.file)
+df2<-df2[,c("Sample", "Class","Status")]
+colnames(df2)<-c("name", "Class","Status")
+df2$name<-paste(df2$name,"_ivar_masked",sep = "")
+
+df<-merge(df, df2, by="name")
+write.table(df, paste(starting.folder, "results/nextclade_and_noise_for_FHI.tsv",sep = ""), row.names = FALSE, quote = FALSE,sep = "\t")
+  
