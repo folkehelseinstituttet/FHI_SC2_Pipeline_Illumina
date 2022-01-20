48e86d47174d4040b5d7520f3896e46ebf2a50d8
Jon Bråte <jon.brate@fhi.no>
2022-01-20 11:38:41 +0100

    Removed old procedure from reading BN from the excel query

--- Scripts/Gisaid_find_plates_ready_for_submission.R
+++ Scripts/Gisaid_find_plates_ready_for_submission.R
@@ -2,15 +2,10 @@
 pacman::p_load(tidyverse, readxl, stringr, lubridate)
 
 # Read data from BioNumerics ----------------------------------------------
+# NB: Husk å kjøre scriptet refresh_data_from_BN.R først 
 try(load(file = "/mnt/N/Virologi/JonBrate/Prosjekter/BN.RData"))
 try(load(file = "N:/Virologi/JonBrate/Prosjekter/BN.RData"))
-# Les inn BN spørring. Husk å Refreshe og lagre den originale excel-fila først (N:/Virologi/Influensa/2021/Spørringsfiler BN/SQLSERVER_TestBN_Spørring_Entrytable.xlsx)
-#BN <- read_excel("/mnt/N/Virologi/Influensa/2021/Spørringsfiler BN/SQLSERVER_TestBN_Spørring_Entrytable.xlsx", sheet = "Sporring BN") %>%
-#  select(KEY, REKVNR, PROVE_TATT, FYLKENAVN, MATERIALE, PROSENTDEKNING_GENOM, DEKNING_NANOPORE, SEKV_OPPSETT_NANOPORE, DEKNING_NANOPORE,
-#         SEKV_OPPSETT_SWIFT7, SEQUENCEID_NANO29, SEQUENCEID_SWIFT, COVERAGE_BREADTH_SWIFT, GISAID_PLATFORM, GISAID_EPI_ISL,
-#         GENOTYPE_SVART_I_LABWARE, COVERAGE_BREATH_EKSTERNE, SAMPLE_CATEGORY, INNSENDER, COVERAGE_DEPTH_SWIFT, COVARAGE_DEPTH_NANO,
-#         RES_CDC_INFA_RX, RES_CDC_INFB_CT, MELDT_SMITTESPORING) %>%
-#  rename("Dekning_Artic" = PROSENTDEKNING_GENOM, "Dekning_Swift" = COVERAGE_BREADTH_SWIFT, "Dekning_Nano" = DEKNING_NANOPORE)
+
 
 # Check Swift FHI
 BN %>%
