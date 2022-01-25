01857da4290d1b0b754d9d1bd23c340f6f8febe9
jonbra <jon.brate@fhi.no>
2022-01-25 14:54:46 +0100

    created file - not ready

---
+++ FHI_Gisaid/Gisaid_create_BioNumerics_import_file.R
@@ -0,0 +1,34 @@
+gisaid_md <- read_tsv(file = "/home/jonr/Downloads/gisaid_hcov-19_2022_01_24_09.tsv")
+
+to_BN_OUS <- gisaid_md %>% 
+  select(`Virus name`, `Accession ID`, `Sequencing technology`) %>% 
+  filter(str_detect(`Virus name`, "OUS")) %>% 
+  separate(`Virus name`, into = c(NA, NA, "Key", NA), sep = "/") %>% 
+  select(Key, 
+         "gisaid_epi_isl" = `Accession ID`,
+         "Platform" = `Sequencing technology`)
+
+write_csv(to_BN, 
+          file = "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/4-GISAIDsubmisjon/2022.01.24_MIK_batch_import.csv",
+          quote = "all")
+
+to_BN_rest <- gisaid_md %>% 
+  filter(str_detect(`Virus name`, "Ahus", negate = TRUE)) %>% 
+  select(`Virus name`, `Accession ID`, `Sequencing technology`) %>% 
+  filter(str_detect(`Virus name`, "OUS", negate = TRUE)) %>% 
+  separate(`Virus name`, into = c(NA, NA, "Key", "year"), sep = "/") %>%
+  # Left pad the Key with zeroes to a total of 5 digits
+  mutate("Key" = str_pad(Key, width = 5, side = c("left"), pad = "0")) %>% 
+  add_column("nr" = 25) %>% 
+  mutate("year" = str_sub(year, 3, 4)) %>% 
+  unite("tmp", c(nr, year, Key), sep = "") %>% 
+  select("Key" = tmp,
+         "gisaid_epi_isl" = `Accession ID`,
+         "Platform" = `Sequencing technology`)
+select(Key, 
+       "gisaid_epi_isl" = `Accession ID`,
+       "Platform" = `Sequencing technology`)
+
+write_csv(to_BN_rest, 
+          file = "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/4-GISAIDsubmisjon/2022.01.24_BN_batch_import.csv",
+          quote = "all")
