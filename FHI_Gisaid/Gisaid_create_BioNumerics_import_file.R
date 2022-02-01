49c729b21d08517ed1c00447df0db24f9971c3e8
jonbra <jon.brate@fhi.no>
2022-02-01 11:08:14 +0100

    Imporved comments

--- FHI_Gisaid/Gisaid_create_BioNumerics_import_file.R
+++ FHI_Gisaid/Gisaid_create_BioNumerics_import_file.R
@@ -1,4 +1,7 @@
-gisaid_md <- read_tsv(file = "/home/jonr/Downloads/gisaid_hcov-19_2022_01_24_09.tsv")
+# Download from Gisaid Sequencing Technology metadata
+
+# Read the metadata file
+gisaid_md <- read_tsv(file = "/home/jonr/Downloads/gisaid_hcov-19_2022_02_01_09.tsv")
 
 to_BN_OUS <- gisaid_md %>% 
   select(`Virus name`, `Accession ID`, `Sequencing technology`) %>% 
@@ -8,13 +11,15 @@ to_BN_OUS <- gisaid_md %>%
          "gisaid_epi_isl" = `Accession ID`,
          "Platform" = `Sequencing technology`)
 
-write_csv(to_BN, 
-          file = "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/4-GISAIDsubmisjon/2022.01.24_MIK_batch_import.csv",
+write_csv(to_BN_OUS, 
+          file = "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/4-GISAIDsubmisjon/2022.02.01_MIK_batch_import.csv",
           quote = "all")
 
 to_BN_rest <- gisaid_md %>% 
+  # Remove Ahus samples
   filter(str_detect(`Virus name`, "Ahus", negate = TRUE)) %>% 
   select(`Virus name`, `Accession ID`, `Sequencing technology`) %>% 
+  # Remove OUS samples
   filter(str_detect(`Virus name`, "OUS", negate = TRUE)) %>% 
   separate(`Virus name`, into = c(NA, NA, "Key", "year"), sep = "/") %>%
   # Left pad the Key with zeroes to a total of 5 digits
@@ -25,10 +30,7 @@ to_BN_rest <- gisaid_md %>%
   select("Key" = tmp,
          "gisaid_epi_isl" = `Accession ID`,
          "Platform" = `Sequencing technology`)
-select(Key, 
-       "gisaid_epi_isl" = `Accession ID`,
-       "Platform" = `Sequencing technology`)
 
 write_csv(to_BN_rest, 
-          file = "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/4-GISAIDsubmisjon/2022.01.24_BN_batch_import.csv",
+          file = "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/4-GISAIDsubmisjon/2022.02.01_BN_batch_import.csv",
           quote = "all")
