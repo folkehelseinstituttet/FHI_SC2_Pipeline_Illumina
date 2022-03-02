828a8cc4f7954d18ab41bb677bc8d6c90a506269
garcia-nacho <garcia.nacho@gmail.com>
2022-03-02 12:50:02 +0100

    UpdateNSC4FHI_nose

--- Scripts/NSC4FHI_noise_nextclade.R
+++ Scripts/NSC4FHI_noise_nextclade.R
@@ -1,6 +1,9 @@
 library(readxl)
 starting.folder<-"/home/docker/Fastq/"
 df<-read.table(paste(starting.folder,"results/nextclade_for_FHI.tsv",sep = ""),sep = "\t")
+summaries.file<-list.files(starting.folder, full.names = TRUE, recursive = TRUE, pattern = "report_v"  )
+
+summaries<-read.csv(summaries.file,sep = "\t") 
 
 if(paste(rownames(df),collapse = "")!=paste(c(1:nrow(df)), collapse="")){
   df<-cbind(rownames(df), df)
@@ -17,5 +20,13 @@ colnames(df2)<-c("name", "Class","Status")
 df2$name<-paste(df2$name,"_ivar_masked",sep = "")
 
 df<-merge(df, df2, by="name")
+
+summaries$name<-paste(summaries$Name,"_ivar_masked",sep = "")
+if(length(which(df$Class=="C" & df$name %in% summaries$name[which(summaries$WGS_median<2500)]))>0){
+df$Class[which(df$Class=="C" & df$name %in% summaries$name[which(summaries$WGS_median<2500)])]<-"LQ"
+}
+if(length(which(df$Class=="C"))>0) df$Class[which(df$Class=="C")]<-"MIX"
+if(length(which(df$Class=="MIX"))>0) df$Status[which(df$Class=="MIX")]<-"PASS"
+
 write.table(df, paste(starting.folder, "results/nextclade_and_noise_for_FHI.tsv",sep = ""), row.names = FALSE, quote = FALSE,sep = "\t")
   
